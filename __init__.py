cd32bfd3c08ed37a5636400838407c5230c52ed2
ziyu huang <huang0ziyu@gmail.com>
2022-03-02 10:25:45 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,39 @@
+from torch import nn
+from torch.nn.modules.loss import *
+from torch.nn.modules.loss import _Loss
+
+from colossalai.nn.layer.utils import get_tensor_parallel_mode 
+from .loss_2d import CrossEntropyLoss2D
+from .loss_2p5d import CrossEntropyLoss2p5D
+from .loss_3d import CrossEntropyLoss3D
+
+_parallel_cross_entropy = {
+    '2d': CrossEntropyLoss2D,
+    '2.5d': CrossEntropyLoss2p5D,
+    '3d': CrossEntropyLoss3D
+}
+
+
+class CrossEntropyLoss(_Loss):
+    def __init__(self, reduction: bool = True, *args, **kwargs):
+        super().__init__()
+        tensor_parallel = get_tensor_parallel_mode()
+        if tensor_parallel in ['None', '1d']:
+            reduction = 'mean' if reduction else 'none'
+            self.loss = nn.CrossEntropyLoss(reduction=reduction, *args, **kwargs)
+        else:
+            self.loss = _parallel_cross_entropy[tensor_parallel](reduction=reduction, *args, **kwargs)
+
+    def forward(self, *args):
+        return self.loss(*args)
+
+
+class MSELoss(_Loss):  # 自己加的，没有考虑其他d的情况，只考虑1d。。。
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        tensor_parallel = get_tensor_parallel_mode()
+        if tensor_parallel in ['None', '1d']:
+            self.loss = nn.MSELoss(*args, **kwargs)
+
+    def forward(self, *args):
+        return self.loss(*args)
